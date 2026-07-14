93b0c8972f5e7b87e69c1ca5b643da21828411cf
iki1uc <entertrainer@web.de>
2026-07-14 02:46:56 +0200

    Create 81_81_1.js

---
+++ 81_81_1.js
@@ -0,0 +1,13 @@
+import { geoMeta } from "./81_81_1.js";
+
+const meta = geoMeta(Phi, phi, phi2, phiinfty);
+
+const FINALCLOU =
+    (3e + LEGO + TETRIZ)
+    * NC.command(meta.axes)
+    * RAW64(meta.lage)
+    * OctaCore3(meta.ort)
+    * (SCHACH + SKAT + ROME + inPIme)
+    * (Civilization + Colonization + Rome2)
+    * 360
+    * 100;
